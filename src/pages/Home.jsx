c8dd4dd8ea7c380dd56c3878f12f72dd52cf232a
Denis Kolomoyets <deniskolomoyets@DenisMac.local>
2024-02-15 20:28:26 +0200

    heroku

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -83,7 +83,9 @@ export const Home = () => {
                 _id={obj._id}
                 title={obj.title}
                 imageUrl={
-                  obj.imageUrl ? `http://localhost:4444${obj.imageUrl}` : ""
+                  obj.imageUrl
+                    ? `${process.env.REACT_APP_API_URL}${obj.imageUrl}`
+                    : ""
                 }
                 createdAt={obj.createdAt}
                 viewsCount={obj.viewsCount}
